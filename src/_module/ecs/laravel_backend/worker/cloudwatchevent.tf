4e8a0347c62c686b3dcbd6c74d1392d10b9e3c7f
S-tanishi <tanichi.u.n.20@gmail.com>
2022-01-18 18:33:58 +0900

    fix: cloud watch event のコンテナ参照先が間違えていたので修正

--- src/_module/ecs/laravel_backend/worker/cloudwatchevent.tf
+++ src/_module/ecs/laravel_backend/worker/cloudwatchevent.tf
@@ -1,6 +1,8 @@
 # ===================================================================
 # cloud watch event
 
+# laravel ではapp側でcron 判定をもたせることができるので定時処理には不要だが、Go とかで使えそうな考えなので残しておく
+
 # worker を毎分ごとに立ち上げている。
 # 立ち上げ時にコマンドが実行されるので結果としてcommandが実行される(heroku側でも毎分コマンドを叩いている仕組み)
 # いつ実行されるかはapplication側で制御している
@@ -10,24 +12,26 @@ data "template_file" "php_artisan_schedule" {
   template = file("../_module/ecs/laravel_backend/worker/ecs_container_overrides.json")
 
   vars = {
-    command = "schedule:run"
+    command = "inspire" # test 用
+    #    command = "schedule:run"
     # option  = "--tries=1"
   }
 }
 
 # 毎分ごとにworkerを起動するルールを定義
 resource "aws_cloudwatch_event_rule" "schedule" {
-  description         = "run php artisan schedule every minutes"
+  description         = "run php artisan schedule at 0:00"
   is_enabled          = true
-  name                = "schedule_every_minutes"
-  schedule_expression = "cron(* * * * ? *)"
+  name                = "schedule_every_days"
+  #  schedule_expression = "cron(* * * * ? *)"
+  schedule_expression = "cron(0 15 * * ? *)"  # 日本時間15+9-> 24(0)時に実行
 }
 
 # workerを毎分操作するためにcloud watch event で司令している
 resource "aws_cloudwatch_event_target" "schedule" {
   rule      = aws_cloudwatch_event_rule.schedule.name
   arn       = var.cluster_arn
-  target_id = "schedule"
+  target_id = "worker" # worker の task 定義を書き換える
   role_arn  = aws_iam_role.ecs_events_run_task.arn
   input     = data.template_file.php_artisan_schedule.rendered
 
